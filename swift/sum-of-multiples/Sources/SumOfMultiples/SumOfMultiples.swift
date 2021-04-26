bc447cc8cc0d315162b920633e948094160c6f2a
Alperen Arıcı <alparicieren@gmail.com>
2021-04-26 10:49:08 +0300

    Sum of multiples

--- swift/sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
+++ swift/sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
@@ -3,7 +3,7 @@
 //
 //If we list all the natural numbers below 20 that are multiples of 3 or 5, we get 3, 5, 6, 9, 10, 12, 15, and 18.
 //
-//The sum of these multiples is 78.
+//The sum of these multiples is 78. //
 import Foundation
 struct SumOfMultiples {
     static func toLimit(_ topNumber: Int, inMultiples: [Int]) -> Int {
